c35922fd2698e74dbaaaff05ff19075055f1f783
jackson12345242 <ryan.brodsky5@icloud.com>
2026-09-12 15:31:26 -0400

    Refactor INITIAL_COGS to remove cogs prefix
    Updated INITIAL_COGS to use simplified module names.

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-INITIAL_COGS = ["cogs.wallet", "cogs.fund"]
+INITIAL_COGS = ["wallet", "fund"]
 
 
 @bot.event
