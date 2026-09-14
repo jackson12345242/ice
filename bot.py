f25c6bdf1ec3e3aceced706b7565300368d89f38
jackson12345242 <ryan.brodsky5@icloud.com>
2026-09-14 18:20:55 -0400

    Add 'split' to initial cogs for the bot

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-INITIAL_COGS = ["wallet", "fund"]
+INITIAL_COGS = ["wallet", "fund", "split"]
 
 
 @bot.event
