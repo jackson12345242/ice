44902b90ebeea72c3eae95f1c6e1fb876211bd5c
jackson12345242 <ryan.brodsky5@icloud.com>
2026-09-26 13:28:53 -0400

    Add 'watchlist' to initial cogs list

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-INITIAL_COGS = ["wallet", "fund", "split", "payment", "help"]
+INITIAL_COGS = ["wallet", "fund", "split", "payment", "help", "watchlist"]
 
 
 @bot.event
